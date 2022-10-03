b9285c62081f44c9804def2b105de546e509452d
Raymond Hurst <rhurst1125@berkeley.edu>
2022-10-03 08:52:55 -0700

    database randomly stopped working and my URL on herou change

--- apps/config.py
+++ apps/config.py
@@ -19,7 +19,10 @@ class Config(object):
     # SQLALCHEMY_DATABASE_URI = 'postgresql://vaxlquiurzpxoy:32aba92d92139e94020a5bbfe0710ef44b225c9534f059772f8aa2bd1dc5f5e4@ec2-44-207-133-100.compute-1.amazonaws.com:5432/df1c3ejse989jg'
 
     # current URL listed on heroku
-    SQLALCHEMY_DATABASE_URI = "postgresql://pyrmhgxikryjsb:9b29330cc961e99da32949916ce8d41da68545beb01bd940ccea7e2e4af475c5@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d7ilia4mf6kmq"
+
+    # URL keeps randomly changing on heroku and I need to understand why. Breaks the app without me having done anything
+    SQLALCHEMY_DATABASE_URI = "postgresql://huvbhkicefylkt:69ae5d5dec2011f7bef3bece224de3e3300c3641b779714091f5980a2e7dd9ee@ec2-3-219-135-162.compute-1.amazonaws.com:5432/d31sdr1nq0s09s"
+
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
